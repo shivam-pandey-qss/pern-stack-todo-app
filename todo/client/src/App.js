8943414323b87fe3698b1ba21b26f27ce57c2581
Shivam Pandey <shivam.pandey@qsstechnosoft.com>
2023-04-25 18:02:09 +0530

    pern todo app complete

---
+++ todo/client/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+
+//components
+import InputTodo from "./components/InputTodo";
+import ListTodo from "./components/ListTodo";
+function App() {
+  return (
+    <> 
+      <div className="container">
+        <InputTodo />
+        <ListTodo />
+      </div>
+    </>
+  );
+}
+
+export default App;
